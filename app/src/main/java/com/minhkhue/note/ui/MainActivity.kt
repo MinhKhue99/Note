35997be8a16e5f55a95e6ad575f1538aa43a3d0a
MinhKhue99 <minhkhuepham992gmail.com>
2021-10-28 17:01:54 +0700

    Setup Project

---
+++ app/src/main/java/com/minhkhue/note/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.minhkhue.note.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minhkhue.note.R
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
